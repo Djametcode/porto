17085828cab523d7b6d0fea9f644a7089fe0a4f3
Djametcode <kampretsiji1@gmai.com>
2022-10-05 21:01:55 +0700

    commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function openSidebar() {
+    const element  = document.getElementById('sidebar');
+    element.style.visibility = 'visible';
+
+    const element2 = document.getElementById('x-btn');
+    element2.style.visibility = 'visible';
+}
+
+function closeSidebar() {
+    const element  = document.getElementById('sidebar');
+    element.style.visibility = 'hidden';
+    const element2 = document.getElementById('x-btn');
+    element2.style.visibility = 'hidden';
+}
+
+function darkMode() {
+    element.style.visibility = 'hidden';
+    const element2 = document.getElementById('x-btn');
+    element2.style.visibility = 'hidden';
+}
